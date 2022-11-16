aa274b727671410d79b6c4d5e6d8af669a3302c9
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-11-16 09:32:15 +0900

    Update EventParticipantService.java

--- src/main/java/com/feelmycode/parabole/service/EventParticipantService.java
+++ src/main/java/com/feelmycode/parabole/service/EventParticipantService.java
@@ -33,6 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = false)
 public class EventParticipantService {
 
     private final EventParticipantRepository eventParticipantRepository;
