ccabb883fb8dc5802095e37376e485f6e0a860c6
choihyeongjun <69442251+choihyeongjun@users.noreply.github.com>
2022-11-19 16:37:09 +0900

    Update run_prod.sh

--- script/run_prod.sh
+++ script/run_prod.sh
@@ -4,7 +4,7 @@ PROJECT_NAME="The Parabole"
 
 TODAY=$(date +"%Y%m%d")
 
-DEPLOY_PATH=/home/ec2-user
+DEPLOY_PATH=/home/ubuntu
 DEPLOY_JAR=the-parabole-event-0.0.1-SNAPSHOT.jar
 DEPLOY_FILE=$DEPLOY_PATH/$DEPLOY_JAR
 
